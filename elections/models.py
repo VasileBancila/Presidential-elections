8ec739963f99796e95f08e68679a2e7f32d1028c
Vasile Bancila <129328240+VasileBancila@users.noreply.github.com>
2024-03-31 23:57:50 +0200

    update DB

--- elections/models.py
+++ elections/models.py
@@ -4,10 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField()
-    #election_candidacy = models.BooleanField(default=False)
-    #voted = models.BooleanField(default=False)
-    #no_votes = models.IntegerField(default=0)
-    
+
     def __str__(self): 
         return self.user.username
     
@@ -19,12 +16,25 @@ class ElectionRound(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Voter(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    election_rounds = models.ManyToManyField(ElectionRound)
+    has_voted = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        unique_together = ("user", "election_round")
 
 class ElectionRanking(models.Model):
     election_round = models.ForeignKey(ElectionRound, on_delete=models.CASCADE)
-    candidate_name = models.CharField(max_length=100)
+    candidate = models.ForeignKey(User, on_delete=models.CASCADE)
     no_votes = models.IntegerField(default=0)
-    voted = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.election_round} - {self.candidate_name}"
+        return f"{self.election_round} - {self.candidate.username}"
+
+    class Meta:
+        unique_together = ("election_round", "candidate")
